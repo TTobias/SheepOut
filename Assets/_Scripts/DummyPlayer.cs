d1ac42801cd0ffe848ec7d6020ba79e4f92f40b6
Philip <33552247+PhilipCDev@users.noreply.github.com>
2021-05-28 22:08:28 +0200

    finished sheep ai

--- Assets/_Scripts/DummyPlayer.cs
+++ Assets/_Scripts/DummyPlayer.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class WayPoint : MonoBehaviour
+public class DummyPlayer : MonoBehaviour
 {
+    public float stealthTimer = 0.8f;
+    public static DummyPlayer instance;
     public Vector3 Position { get { return transform.position; } }
+
+    private void Awake()
+    {
+        instance = this;    
+    }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.green;
         Gizmos.DrawSphere(transform.position, 0.2f);
     }
 
