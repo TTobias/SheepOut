d9619385981af9425a8551225fbae9555d1b65d9
Julia <wolfj@hochschule-trier.de>
2021-05-29 18:36:47 +0200

    MainMenu hinzugefügt (Platzhaltergrafiken)

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject creditsOverlay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsOverlay.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void OpenCredits()
+    {
+        creditsOverlay.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsOverlay.SetActive(false);
+    }
+}
