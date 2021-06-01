6869a86402abb2d84e77bfa852f82d2946e36637
Sebastian Krause <sebastian@battyRabbit.com>
2021-06-01 18:08:23 +0200

    Normalize Movement (same speed all directions)

--- Assets/_Scripts/WolfController.cs
+++ Assets/_Scripts/WolfController.cs
@@ -76,6 +76,7 @@ public class WolfController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
+        move = move.normalized;
 
         bool oldMoving = bLateralMoving;
         bLateralMoving = move.sqrMagnitude > 0.0F;
